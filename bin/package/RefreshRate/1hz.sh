d868edcd0e881a9cadb55b54ee40116d693002bc
tiencv2006 <tiencv102006@gmail.com>
2026-05-29 03:27:53 +0700

    Update 1hz.sh

--- bin/package/RefreshRate/1hz.sh
+++ bin/package/RefreshRate/1hz.sh
@@ -4,22 +4,24 @@ gfFile="$work_dir/build/baserom/images/product/etc/device_features/*.xml"
 checkfps='<bool name="support_smart_fps">true</bool>'
 
 
-mods "Adding SmartFPS..."
-for gfFile in $gfFile; do
 if [ `grep -c "$checkfps" $gfFile` -eq '0' ];then
+    for gfFile in $gfFile; do
+    mods "Adding SmartFPS..."
     sed '/<integer name="defaultFps">60<\/integer>/a\    <bool name="support_smart_fps">true<\/bool>\<integer name="smart_fps_value">120<\/integer>' $gfFile > ${gfFile}.new
     mv ${gfFile}.new $gfFile
-	echo "Added SmartFPS to ${gfFile}"
+    done
+    mods "Added SmartFPS"
+else
+    mods "Already have SmartFPS"
 fi
-done
 
-mods "Adding 1hz..."
+mods "Adding more refresh rates..."
 for i in "$gfFile"; do
   [ -f "$i" ] || continue
-  grep -q '<item>120</item>' "$i" || sed -i '/<item>144<\/item>/a\        <item>120<\/item>' "$i"
-  grep -q '<item>90</item>' "$i" || sed -i '/<item>120<\/item>/a\        <item>90<\/item>' "$i"
-  grep -q '<item>1</item>' "$i" || sed -i '/<item>60<\/item>/a\        <item>1<\/item>' "$i"
+  grep -q '<item>120</item>' "$i" || mods "Adding 120hz Option..." || sed -i '/<item>144<\/item>/a\        <item>120<\/item>' "$i"
+  grep -q '<item>90</item>' "$i" || mods "Adding 90hz Option..." || sed -i '/<item>120<\/item>/a\        <item>90<\/item>' "$i"
+  grep -q '<item>1</item>' "$i" || mods "Adding 1hz Option..." || sed -i '/<item>60<\/item>/a\        <item>1<\/item>' "$i"
   sed -i 's/>CN</>ALL</g' "$i"
-  grep -q '<bool name="support_aod_fullscreen">true</bool>' "$i" || sed -i '/<\/features>/i\    <bool name="support_aod_fullscreen">true<\/bool>' "$i"
+  grep -q '<bool name="support_aod_fullscreen">true</bool>' "$i" || mods "Adding AOD Fullscreen..." || sed -i '/<\/features>/i\    <bool name="support_aod_fullscreen">true<\/bool>' "$i"
 done
 mods "Done"
